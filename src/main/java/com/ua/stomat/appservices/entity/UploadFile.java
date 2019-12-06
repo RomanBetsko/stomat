f7a4cc924a8f6821bb50edbde0914c55904a36d2
romanbetsko <rbetsko@intry.careers>
2019-12-06 15:40:59 +0200

    new user site

--- src/main/java/com/ua/stomat/appservices/entity/UploadFile.java
+++ src/main/java/com/ua/stomat/appservices/entity/UploadFile.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 public class UploadFile {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "file_id")
     private String fileId;
     @Column(name = "file_name")
